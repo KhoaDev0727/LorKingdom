b053d434d2d668fd19a8c625ffa75c9fbce5f607
KhangKJ1502 <125472792+KhangKJ1502@users.noreply.github.com>
2025-02-13 10:13:39 +0700

    Push Gop

--- src/java/DBConnect/DBConnection.java
+++ src/java/DBConnect/DBConnection.java
@@ -5,10 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static String url = "jdbc:sqlserver://localhost:1433;databaseName=LorKingDom;encrypt=true;trustServerCertificate=true";
-    private static String user = "sa";
-    private static String pass = "123456789";
+//    private static String url = "jdbc:sqlserver://localhost:1433;databaseName=LorKingDom;encrypt=true;trustServerCertificate=true";
+//    private static String user = "sa";
+//    private static String pass = "123456789";
     
+        private static String url = "jdbc:sqlserver://localhost:1433;databaseName=LorKingDom;encrypt=true;trustServerCertificate=true";
+    private static String user = "sa";
+    private static String pass = "khangmc1502@";
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Connection conn = DBConnection.getConnection();
         if (conn != null) {
