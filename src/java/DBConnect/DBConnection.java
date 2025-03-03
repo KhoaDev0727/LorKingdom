b5a2b289b94b1ebce72c6b2987e5386777cde307
Quynh <quynhchung909@gmail.com>
2025-03-03 13:35:20 +0700

    Update shipping, payment

--- src/java/DBConnect/DBConnection.java
+++ src/java/DBConnect/DBConnection.java
@@ -10,18 +10,18 @@ public class DBConnection {
 //    private static String user = "sa";
 //    private static String pass = "12345";
     
-//    private static String url = "jdbc:sqlserver://QUIN;databaseName=LorKingDom;encrypt=true;trustServerCertificate=true";
-//    private static String user = "sa";
-//    private static String pass = "12345";
+    private static String url = "jdbc:sqlserver://QUIN;databaseName=LorKingDom;encrypt=true;trustServerCertificate=true";
+    private static String user = "sa";
+    private static String pass = "12345";
   
   
 //    private static String url = "jdbc:sqlserver://LEMINHKHOA:1433;databaseName=LorKingDom;encrypt=true;trustServerCertificate=true";
 //    private static String user = "sa";
 //    private static String pass = "12345";   
 
-    private static String url = "jdbc:sqlserver://localhost:1433;databaseName=LorKingDom;encrypt=true;trustServerCertificate=true";
-    private static String user = "sa";
-    private static String pass = "khangmc1502@";
+//    private static String url = "jdbc:sqlserver://localhost:1433;databaseName=LorKingDom;encrypt=true;trustServerCertificate=true";
+//    private static String user = "sa";
+//    private static String pass = "khangmc1502@";
 //
 //    private static String url = "jdbc:sqlserver://localhost:1433;databaseName=LorKingDom;encrypt=true;trustServerCertificate=true";
 //    private static String user = "sa";
